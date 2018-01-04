d89bfa38b7f64072c6a09ef908c2dd4ef6433bef
Analytics.Raju@GMail.com <31151331+analyticsraju@users.noreply.github.com>
2018-01-04 18:03:08 +0530

    NLP Lemmatizing _Stemming codes
    NLP Lemmatizing _Stemming codes

---
+++ 3.NLP/1.NLP_Exercises/NLP_Lemmatizing_Stemming.py
@@ -0,0 +1,23 @@
+from nltk.stem import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+ps = PorterStemmer()
+list_words = ["python","pythoner","pythoning","pythoned","pythonly","wolves", "leaves","horses","dogs","fairly",
+              'dogs', 'leaves','wolves','babies','geese']
+for word in list_words:
+    print(ps.stem(word))
+    
+
+from nltk import stem
+wn_lemat = stem.WordNetLemmatizer()
+for word in list_words:
+    print(word)
+    print('steming==>', ps.stem(word))
+    print('lemmati==>', wn_lemat.lemmatize(word))
+
+
+#Conclusion:
+    #Lemmatization have more accuracy than Stemming
+    #Lemmatization gives almost Ditcionary or meaningful word
+    #Stemming gives base form of word either it could be wrong or right.
+
+ 
