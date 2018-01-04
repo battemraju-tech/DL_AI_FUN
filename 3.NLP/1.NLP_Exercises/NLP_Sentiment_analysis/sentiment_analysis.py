3f9ab09f05905b01ac3cefc4bcddd5d2141db815
Analytics.Raju@GMail.com <31151331+analyticsraju@users.noreply.github.com>
2018-01-04 18:36:41 +0530

    nlp sentiment analysis
    nlp sentiment analysis

---
+++ 3.NLP/1.NLP_Exercises/NLP_Sentiment_analysis/sentiment_analysis.py
@@ -0,0 +1,22 @@
+import os
+
+os.chdir('D:/Data/DataScience/DeepLearning/Natural Language Processing/NLP_handson/training_data/')
+sentiment_dict = {}
+for each_line in open('sentiment_analysis.txt'):
+    word,score = each_line.split('\t')
+    sentiment_dict[word] = int(score)
+    
+user_comment = 'This is a good training and i appreciate it. But lab machines are worst'.lower().split()
+
+print(user_comment)
+print('over all user rating==>', 
+      sum( sentiment_dict.get(word, 0) for word in user_comment ))
+
+print(sentiment_dict.get('good', 0))
+print(sentiment_dict.get('appreciate', 0))
+print(sentiment_dict.get('worst', 0))
+
+for word in user_comment:
+    sentiment_dict.get(word,0)
+    print(word, 'count', sentiment_dict.get(word,0))
+    
